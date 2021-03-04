45583e76203fed66deb5e51c6c98450cc5bbc9c6
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2021-03-03 23:29:09 -0300

    Projeto criado

---
+++ src/test/java/br/com/alexandrepalota/listadecompras/ListaDeComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alexandrepalota.listadecompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
